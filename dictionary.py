63006b6f7e15db456e66d230fafe8fecb2799c11
Niman-06 <nimanpreetkaur04@gmail.com>
2026-06-24 18:04:50 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+# a={"name":"niman",
+#    "from" :"India",
+#    "marks":[90,78,88]}
+# print(a.popitem)
+# print(a.items())
+# print(a.keys())
+# print(a.values())
+# a.update({"friends":"raman"})
+# print(a)
+# print(a.update({"subject":"hindi"}))
+# print(a.get("marks"))
+# print(a["marks"])
+# a.pop("from")
+# print(a)
+# s={18,"18"}
+# print(s)
+s=set()
+user1=int(input("enter your number:"))
+s.add(user1)
+user2=int(input"enter your number")
+s.add(user2)
+print(s)
