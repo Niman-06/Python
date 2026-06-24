be82f136dc96a61a19bdea4f3bbe30413206b692
Niman-06 <nimanpreetkaur04@gmail.com>
2026-06-24 18:06:34 +0530

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,42 @@
+# tupple=("hi",2,9.0,"no")
+# print(type(tupple))
+
+# l1=[10,50,30]
+# print(sorted(l1))
+
+
+
+# print(min(l1))
+# j=(917,45,)
+# print(min(j))
+
+# l1=[]
+# l2=input("enter fruit name:")
+# l1.append(l2)
+# l3=input("enter fruit name:")
+# l1.append(l3)
+# l4=input("enter fruit name:")
+# l1.append(l4)
+
+# print(l1)
+
+# l1=[]
+# a=int(input("enter your marks:"))
+# l1.append(a)
+# b=int(input("enter your marks:"))
+# l1.append(b)
+# c=int(input("enter your marks:"))
+# l1.append(c)
+
+# print(sorted(l1))
+# l1=[]
+# try:
+# a=int(input("enter your number:"))
+# print(a)
+# l1.append(a)
+# except :
+# print("not valid")
+# num=[10,20,40,55]
+# num.remove(20)
+# num.pop(1)
+# print(num)
